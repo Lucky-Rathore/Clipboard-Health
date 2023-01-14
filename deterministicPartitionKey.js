3484dbd38cb56c70dd937de7d80bab18871f3d86
Lucky Rathore <luckyrathore03@gmail.com>
2023-01-14 10:14:09 +0530

    Update deterministicPartitionKey.js

--- deterministicPartitionKey.js
+++ deterministicPartitionKey.js
@@ -1,35 +1,37 @@
 const crypto = require("crypto");
 
-function deterministicPartitionKey(event){
+//Input: event object
+//Return:"partition key" 
+//Return Type: String
+//step 1: function return TRIVIAL_PARTITION_KEY("0") if event is null 
+//step 2: if event.partitionKey if it exist 
+//step 3: return hash value of "stringify event" if event.partitionKey not exist. (uses sha3-512 algorithm)
+//step 4: if returning key length is greater then 256, Hash value of key is returned. 
+function deterministicPartitionKey(event) {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  let partitionKey;
 
   if (event) {
     if (event.partitionKey) {
-      candidate = event.partitionKey;
-      console.log(1);
+      partitionKey = event.partitionKey;
     } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-      console.log(2);
+      const stringfyEvent = JSON.stringify(event);
+      partitionKey = crypto.createHash("sha3-512").update(stringfyEvent).digest("hex");
     }
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-      console.log(3);
+  if (partitionKey) {
+    if (typeof partitionKey !== "string") {
+      partitionKey = JSON.stringify(partitionKey);
     }
   } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-    console.log(4);
+    partitionKey = TRIVIAL_PARTITION_KEY;
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-    console.log(5);
+  if (partitionKey.length > MAX_PARTITION_KEY_LENGTH) {
+    partitionKey = crypto.createHash("sha3-512").update(partitionKey).digest("hex");
   }
-  return candidate;
+  return partitionKey;
 };
 
 module.exports = deterministicPartitionKey;
