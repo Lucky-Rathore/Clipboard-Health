14b2d81a9777a6a5f1410097bd8981e37637d741
Lucky Rathore <luckyrathore03@gmail.com>
2023-01-14 10:03:33 +0530

    unit test

--- deterministicPartitionKey.test.js
+++ deterministicPartitionKey.test.js
@@ -11,3 +11,6 @@ test('if partition key is a integer', () => {  expect(deterministicPartitionKey(
 
 test('if event is null', () => {  expect(deterministicPartitionKey(null)).toBe("0");
 });
+
+test('if partition key length is greater than 256', () => {  expect(deterministicPartitionKey({"partitionKey":"c1802e6b9670927ebfddb7f67b3824642237361f07db35526c42c555ffd2dbe74156c366e1550ef8c0508a6cc796409a7194a59bba4d300a6182b483d315a862c1802e6b9670927ebfddb7f67b3824642237361f07db35526c42c555ffd2dbe74156c366e1550ef8c0508a6cc796409a7194a59bba4d300a6182b483d315a862c1802e6b9670927ebfddb7f67b3824642237361f07db35526c42c555ffd2dbe74156c366e1550ef8c0508a6cc796409a7194a59bba4d300a6182b483d315a862"})).toBe("dbdbea1dfd870ff4c4174b3331d7d332776fdbf2438dc7c910d2c889ca98a62eccb077cac648a3fa40e59362d2f393cf41a7ee3cf9d29a567b74c8353a3d0abc");
+});
